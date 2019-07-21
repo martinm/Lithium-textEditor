6532b6dfd8d3538be12e87229da15fade509d7c0
martinm <malinowskimn@gmail.com>
2019-07-21 15:57:57 +0100

    setting up

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const fs = require('fs')
+
+let mainWindow
+
+createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
